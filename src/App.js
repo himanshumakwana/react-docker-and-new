6621d886829cd6405f344520a88b0e0ac5307cbb
Himanshu Makwana <himanshu.accreteinfo@gmail.com>
2022-11-10 18:51:57 +0530

    view selected pic in ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import ErrorBountry from "./error-bountry/ErrorBountry";
 import LazyImport from "./lazy-suspense/LazyImport";
 
 function App() {
+  const [picture, setPicture] = useState()
+
+  const changeFile = (e) => {
+    console.log(URL.createObjectURL(e.target.files[0]))
+    setPicture(URL.createObjectURL(e.target.files[0]))
+  }
+
   return (
     <div className="App">
       {/* <ErrorBountry /> */}
+
+      <form>
+        <img src={`${picture}`} alt="nothing here" />
+        <input type="file" name="file" onChange={changeFile} />
+      </form>
+
       <LazyImport />
     </div>
   );
