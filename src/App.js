36aba877e1e2268b1fe3c27edb5bea8df744b07c
Himanshu Makwana <himanshu.accreteinfo@gmail.com>
2022-11-11 13:31:45 +0530

    changes for fsd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ErrorBountry from "./error-bountry/ErrorBountry";
 import LazyImport from "./lazy-suspense/LazyImport";
 
 function App() {
   const [picture, setPicture] = useState()
+  
+  var [value, setValue] = useState()
+  
+  console.log("first")
+  console.log("value1")
+
+  useEffect(() => {
+    console.log("value1")
+  }, [])
+  
 
   const changeFile = (e) => {
     console.log(URL.createObjectURL(e.target.files[0]))
     setPicture(URL.createObjectURL(e.target.files[0]))
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    value = "value"
+
+    setValue(val => {
+      console.log("function value", value)
+    })
+    console.log("value3")
   }
 
   return (
@@ -17,6 +37,7 @@ function App() {
       <form>
         <img src={`${picture}`} alt="nothing here" />
         <input type="file" name="file" onChange={changeFile} />
+        <button onClick={handleClick}>Click me</button>
       </form>
 
       <LazyImport />
